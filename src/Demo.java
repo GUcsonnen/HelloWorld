fa875217d58d6bb276818339b79179b28f619604
CarolineSonnen <csonnen@zagmail.gonzaga.edu>
2019-08-29 10:13:54 -0700

    added new fun println

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,8 @@
 public class Demo {
     public static void main(String[] args){
-        System.out.println("HelloWorld");
-        System.out.println("Class is fun!");
-        System.out.println("Coding is cool");
+        System.out.println("HelloWorld!");
+        System.out.println("Class is fun!!");
+        System.out.println("Coding is cool!!!");
     }
 }
 
