7363a47ba68e302879e76b154db369b8b26f981e
CarolineSonnen <csonnen@zagmail.gonzaga.edu>
2019-08-29 10:11:10 -0700

    inserted new println

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,7 @@ public class Demo {
     public static void main(String[] args){
         System.out.println("HelloWorld");
         System.out.println("Class is fun!");
+        System.out.println("Coding is cool");
     }
 }
 
